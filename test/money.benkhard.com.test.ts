d19eaa403a8f63a83882effdd6d7f3490c56afc0
Timo Benkhard <tcbenkhard@gmail.com>
2025-01-06 15:01:21 +0100

    Initial commit

---
+++ test/money.benkhard.com.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MoneyBenkhardCom from '../lib/money.benkhard.com-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/money.benkhard.com-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MoneyBenkhardCom.MoneyBenkhardComStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
